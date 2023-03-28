59ccf793ec5e57f72b418f7854813c4dc6cb75fc
Tim <2760569447csy@gmail.com>
2023-03-28 00:09:31 -0700

    Fixed blob

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,23 @@
-// Add blob to the body
-var blob = document.createElement("div");
-blob.classList.add("blob");
-document.body.appendChild(blob);
+'use strict';
 
-// Update blob position on mouse move
-document.addEventListener("mousemove", function(e) {
-	blob.style.left = e.clientX + "px";
-	blob.style.top = e.clientY + "px";
+
+
+document.addEventListener("mousemove", function (event) {
+    let blob = document.getElementById("blob");
+    let x = event.clientX - (blob.clientWidth / 2);
+    let y = event.clientY - (blob.clientHeight / 2);
+    blob.style.left = x + "px";
+    blob.style.top = y + "px";
+});
+
+
+
+// Set the cursor to be in the center of the blob
+
+document.addEventListener("mousemove", function (event) {
+    let x = event.clientX - blob.offsetWidth / 2;
+    let y = event.clientY - blob.offsetHeight / 2;
+    blob.style.left = x + "px";
+    blob.style.top = y + "px";
+    blob.style.cursor = "none";
 });
